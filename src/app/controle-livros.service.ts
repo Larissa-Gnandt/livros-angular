c1d8376d3a47ef529b10faa318e317aeea3c8470
Larissa <larissagnandt@gmail.com>
2024-10-29 20:45:13 -0300

    Codificação do controlador de livros, no arquivo controle-livros.service.ts

--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Livro } from './livro';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ControleLivrosService {
+  private livros: Array<Livro> = [
+    {
+      codigo: 1,
+      codEditora: 1,
+      titulo: 'Livro 1',
+      resumo: 'Resumo do Livro 1',
+      autores: ['Autor 1'],
+    },
+    {
+      codigo: 2,
+      codEditora: 2,
+      titulo: 'Livro 2',
+      resumo: 'Resumo do Livro 2',
+      autores: ['Autor 2'],
+    },
+    {
+      codigo: 3,
+      codEditora: 3,
+      titulo: 'Livro 3',
+      resumo: 'Resumo do Livro 3',
+      autores: ['Autor 3'],
+    },
+  ];
 
-  constructor() { }
+  obterLivros(): Array<Livro> {
+    return this.livros;
+  }
+
+  incluir(livro: Livro): void {
+    livro.codigo = this.livros.length
+      ? Math.max(...this.livros.map((l) => l.codigo)) + 1
+      : 1;
+    this.livros.push(livro);
+  }
+
+  excluir(codigo: number): void {
+    const index = this.livros.findIndex((l) => l.codigo === codigo);
+    if (index !== -1) {
+      this.livros.splice(index, 1);
+    }
+  }
 }
