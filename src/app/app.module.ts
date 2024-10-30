c1458c770408e2411366cf8a4c8db385d8b04213
Larissa <larissagnandt@gmail.com>
2024-10-30 18:43:22 -0300

    Ajustes no estilo de Dados do Livro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LivroListaComponent } from './livro-lista/livro-lista.component';
 import { ControleEditoraService } from './controle-editora.service';
+import { LivroDadosComponent } from './livro-dados/livro-dados.component';
 import { ControleLivrosService } from './controle-livros.service';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -10,9 +11,11 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [],
   imports: [
     BrowserModule,
     LivroListaComponent,
+    LivroDadosComponent,
     FormsModule,
     RouterModule.forRoot(routes),
     CommonModule,
