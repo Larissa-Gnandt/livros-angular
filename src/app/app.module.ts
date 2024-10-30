3492123f6ef376e040597613c0ce5bfa55120dce
Larissa <larissagnandt@gmail.com>
2024-10-30 15:25:46 -0300

    Configuração do roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LivroListaComponent } from './livro-lista/livro-lista.component';
 import { ControleEditoraService } from './controle-editora.service';
 import { ControleLivrosService } from './controle-livros.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [BrowserModule, LivroListaComponent],
+  imports: [BrowserModule, LivroListaComponent, FormsModule],
   providers: [ControleEditoraService, ControleLivrosService],
 })
 export class AppModule {}
